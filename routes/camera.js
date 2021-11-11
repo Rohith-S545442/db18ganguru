239804be97c2b2bb7c4fa83fc4c095ef870a2942
Rohith-S545442 <rohitsai.ganguru1997@gmail.com>
2021-11-10 22:52:24 -0600

    Added camera controlers in camera routes

--- routes/camera.js
+++ routes/camera.js
@@ -1,8 +1,13 @@
 var express = require('express');
+const camera_controlers= require('../controllers/camera');
 var router = express.Router();
 
+/* GET camera */
+router.get('/', camera_controlers.camera_view_all_Page );
+module.exports = router;
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('camera', { title: 'Search Results Camera' });
 });
 
