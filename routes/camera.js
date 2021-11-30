9b6aef18d17f9995d077355d6c4653c4d5084fba
Rohith-S545442 <rohitsai.ganguru1997@gmail.com>
2021-11-30 12:19:16 -0600

    added login, Registeer and secure

--- routes/camera.js
+++ routes/camera.js
@@ -2,6 +2,14 @@ var express = require('express');
 const camera_controlers = require('../controllers/camera');
 var router = express.Router();
 
+const secured = (req, res, next) => { 
+  if (req.user){ 
+    return next(); 
+  } 
+  req.session.returnTo = req.originalUrl; 
+  res.redirect("/login"); 
+} 
+
 /* GET camera */
 router.get('/', camera_controlers.camera_view_all_Page);
 module.exports = router;
@@ -16,7 +24,7 @@ router.get('/detail', camera_controlers.camera_view_one_Page);
 router.get('/create', camera_controlers.camera_create_Page);
 
 /* GET create update page */
-router.get('/update', camera_controlers.camera_update_Page);
+router.get('/update',secured, camera_controlers.camera_update_Page);
 
 /* GET create camera page */
 router.get('/delete', camera_controlers.camera_delete_Page);
